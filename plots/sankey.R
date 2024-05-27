457b18aa28d89171e0877134f00a5ba2194c73b9
Sehrish <kanwals@unimelb.edu.au>
2024-05-27 18:04:29 +1000

    try sankey plot for diagnostic dilemma categories

--- plots/sankey.R
+++ plots/sankey.R
@@ -14,10 +14,16 @@ nodes <- data.frame(name = c(
   "Lack of Clinician Buy-In (1)"
 ))
 
+#links <- data.frame(
+#  source = c(0, 0, 0, 0, 0, 0, 0, 0, 0),
+#  target = c(1, 2, 3, 4, 5, 6, 7, 8, 9),
+#  value = c(44, 1, 3, 5, 1, 4, 3, 1, 0)
+#)
+
 links <- data.frame(
-  source = c(0, 0, 0, 0, 0, 0, 0, 0, 0),
-  target = c(1, 2, 3, 4, 5, 6, 7, 8, 9),
-  value = c(44, 1, 3, 5, 1, 4, 3, 1, 0)
+  source = c(1,2,3,4,5,2),
+  target = c(1,1,1,1,1,2),
+  value =  c(8,4,1,1,2)
 )
 
 # Sorting links by value in descending order
@@ -47,8 +53,65 @@ layout <- list(
 )
 
 # Create and display the final plot
-sankey_plot <- sankey_plot %>% layout(layout)
+sankey_plot <- sankey_plot %>% plotly::layout(layout)
+htmlwidgets::saveWidget(sankey_plot, file = "/Users/kanwals/UMCCR/research/projects/column-pi/sankey_plot.html")
 
+# Plot diagnostic dilemma categories
+# Define the data as text and read csv
+csv_data <- "
+source,    target,    value,  group
+One_1,     One_2,     8,    mono
+One_1,     One_2,     3,    pleo
+One_1,     One_2,     7,    rcc
+One_1,     One_2,     2,    swi
+Two_1,     One_2,     4,    mono
+Two_1,     Two_2,     1,    mono
+Two_1,     One_2,     4,    pleo
+Two_1,     One_2,     4,    swi
+Two_1,     Two_2,     1,    pleo
+Three_1,     One_2,   1,    mono
+Three_1,     One_2,   4,    pleo
+Three_1,     Two_2,   1,  pleo
+Four_1,   One_2,   1,   mono
+Four_1,   One_2,   2,   pleo
+Four_1,   Three_2,   1,   pleo
+Five_1,   One_2,    2,    mono
+"
+links <- read.csv(text = csv_data, header = TRUE, strip.white = TRUE)
 
+# Extract unique node IDs from source and target columns
+id <- unique(c(as.character(links$source), as.character(links$target)))
 
+# Create labels by removing the suffix
+label <- sub("_[0-9]", "", id)
+
+# Create nodes data frame
+nodes <- data.frame(id = id, label = label)
+
+# Add a 'group' column to each node. Here I decided to put all of them in the same group to make them grey
+# reference https://r-graph-gallery.com/322-custom-colours-in-sankey-diagram.html
+nodes$group <- as.factor(c("my_unique_group"))
+
+# Update links data frame to add id sourcw and target columns
+links$IDsource <- match(links$source, nodes$id) - 1
+links$IDtarget <- match(links$target, nodes$id) - 1
+
+# Define color scale for groups
+color_scale <- 'd3.scaleOrdinal()
+                .domain(["mono", "pleo", "rcc", "swi", "my_unique_group"])
+                .range(["#69b3a2", "#b3697a", "#b37d69", "#a269b3", "grey"])'
+
+# Create Sankey network diagram
+sankeyNetwork(
+  Links = links,
+  Nodes = nodes,
+  Source = "IDsource",
+  Target = "IDtarget",
+  Value = "value",
+  NodeID = "label",
+  fontSize = 12,
+  colourScale = color_scale,
+  LinkGroup = "group",
+  NodeGroup = "group"
+)
 
